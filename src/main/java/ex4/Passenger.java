9d1004827aaacad3a2130df8d45abb7fd10e184e
Huyen-ntt <nguyenhuyenpy98@gmail.com>
2021-09-01 13:37:18 +0700

    Ex4

---
+++ src/main/java/ex4/Passenger.java
@@ -0,0 +1,57 @@
+package ex4;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Passenger {
+    private String hoten;
+    private boolean gioiTinh;
+    private int tuoi;
+    private Ticket[] danhSachVe;
+
+    public Passenger(String hoten, boolean gioiTinh, int tuoi, Ticket[] danhSachVe) {
+        this.hoten = hoten;
+        this.gioiTinh = gioiTinh;
+        this.tuoi = tuoi;
+        this.danhSachVe = danhSachVe;
+    }
+
+//    public Passenger() {
+//
+//    }
+
+    @Override
+    public String toString() {
+        String s="";
+        for (Ticket i: danhSachVe) {
+            s+= i.toString()+"\n";
+        }
+        return "Passenger: " + "hoten: '" + hoten + '\'' +
+                ", gioiTinh: " + gioiTinh +
+                ", tuoi: " + tuoi +
+                ", danhSachVe:" +"\n"+  s ;
+
+    }
+
+    public int tinhTongTien() {
+        int result=0;
+        for (Ticket i:this.danhSachVe) {
+            result+= i.getGiaVe();
+        }
+        return result;
+    }
+    public  void sortDESC() {
+        Ticket temp = this.danhSachVe[0];
+        for ( int i= 0 ; i < this.danhSachVe.length - 1; i++) {
+            for (int j = i + 1; j < this.danhSachVe.length; j++) {
+                if (this.danhSachVe[i].getGiaVe() < this.danhSachVe[j].getGiaVe()) {
+                    temp = this.danhSachVe[j];
+                    this.danhSachVe[j] = this.danhSachVe[i];
+                    this.danhSachVe[i] = temp;
+                }
+            }
+        }
+    }
+
+}
---
+++ src/main/java/ex4/Ticket.java
@@ -0,0 +1,31 @@
+package ex4;
+
+import java.util.Date;
+import java.util.ArrayList;
+
+public class Ticket {
+    private String tenChuyen;
+    private Date ngayBay;
+    private int giaVe;
+
+    public Ticket(String tenChuyen, Date ngayBay, int giaVe) {
+        this.tenChuyen = tenChuyen;
+        this.ngayBay = ngayBay;
+        this.giaVe = giaVe;
+    }
+
+    public Ticket() {
+
+    }
+
+    public int getGiaVe() {
+        return giaVe;
+    }
+
+    @Override
+    public String toString() {
+        return "Ticket: " + "tenChuyen='" + tenChuyen + '\'' + ", ngayBay=" + ngayBay + ", giaVe=" + giaVe;
+    }
+
+
+}
