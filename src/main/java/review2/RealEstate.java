100142f2587a7a3c22df26a11f58c033b4066c5d
Huyen-ntt <nguyenhuyenpy98@gmail.com>
2021-09-11 10:03:52 +0700

    Review 12

--- src/main/java/review2/RealEstate.java
+++ src/main/java/review2/RealEstate.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 public class RealEstate {
     public static void main(String[] args) {
+        double price=500000.0 ;
         Square s = new Square(2.0, 7.0, 6.2);
         Circle c = new Circle(3.0, 7.3);
         Geometry g = new Geometry(0.0, 6.0);
@@ -17,9 +18,9 @@ public class RealEstate {
         int total = 0;
         for (Rectangular r : list) {
             System.out.println(r);
-            total += r.area();
+            total += price*r.area();
         }
-        System.out.println("Tổng tiền phải chi trả sở hữu các mảnh đất: " + total);
+        System.out.println("Tổng tiền phải chi trả sở hữu các mảnh đất: " + total+ " VNĐ");
 
     }
 }
