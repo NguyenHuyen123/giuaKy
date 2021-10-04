ea5e75155831e625c37916d0b6716c3d7b931514
Huyen-ntt <nguyenhuyenpy98@gmail.com>
2021-10-04 23:06:52 +0700

    Java IO

--- src/main/java/javaIO/TraverseDepthFiles.java
+++ src/main/java/javaIO/TraverseDepthFiles.java
@@ -1,10 +1,17 @@
 package javaIO;
 
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.Arrays;
 import java.util.Comparator;
 
 public class TraverseDepthFiles {
+    // 1. Bài tập: liệt kê cây thư mục của thư mục hiện hành (user.dir)
     public void traverseDepthFiles(final File fileOrDir) {
         if (fileOrDir.isDirectory()) {
             System.out.println(fileOrDir.getAbsolutePath());
@@ -24,5 +31,46 @@ public class TraverseDepthFiles {
         } else {
             System.out.println(fileOrDir.getAbsolutePath());
         }
+    }
+
+    private static String readFile(String filePath) {
+        String content = "";
+
+        try {
+            content = new String(Files.readAllBytes(Paths.get(filePath)));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return content;
+    }
+
+    private String writeFile(String content, String path) {
+        try {
+            File file = new File(path);
+
+            if (!file.exists()) {
+                file.createNewFile();
+            }
+
+            FileWriter fw = new FileWriter(file.getAbsoluteFile());
+            BufferedWriter bw = new BufferedWriter(fw);
+
+            bw.write(content);
+
+            bw.close();
+            return content;
+        } catch (Exception e) {
+            System.out.println(e);
+            return "";
+        }
+    }
+
+
+    public static void main(String[] args) {
+		File fileOrDir = new File("D:\\");
+        TraverseDepthFiles ex1 = new TraverseDepthFiles();
+    	ex1.traverseDepthFiles(fileOrDir);
+
     }
 }
